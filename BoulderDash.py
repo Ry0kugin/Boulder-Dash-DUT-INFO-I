6120aca2c4c29be5ae9145317dbdd0e2c1538b96
Ry0kugin <ryokugin.code@gmail.com>
2019-11-11 14:12:52 +0100

    add correction reset win physics

--- BoulderDash.py
+++ BoulderDash.py
@@ -2,25 +2,27 @@
 
 from upemtk import *
 from time import *
+from random import randint
 
 ###### Constants used for this game #######
-LEVEL_1 = [
-        ['150s', '1d'],
-        ["W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W",],
-        ["W", "G", "G", "G", "G", "G", "G", ".", "G", "G", "D", "G", "B", "W",],
-        ["W", "G", "B", "R", "B", "G", "G", "G", "G", "G", "G", ".", "G", "W",],
-        ["W", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", ".", "G", "W",],
-        ["W", "B", "G", "B", "B", "G", "G", "G", "G", "G", "G", "G", "G", "W",],
-        ["W", "G", "G", ".", "B", "G", "G", "G", "G", "G", "G", "G", "G", "W",],
-        ["W", "G", "G", "G", ".", "G", "G", ".", "G", "G", "G", "G", "E", "W",],
-        ["W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W",]
-]
+    # level = [
+    #     ['150s', '1d'],
+    #     ["W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W",],
+    #     ["W", "G", "G", "G", "G", "G", "G", ".", "G", "G", "D", "G", "B", "W",],
+    #     ["W", "G", "B", "R", "B", "G", "G", "G", "G", "G", "G", ".", "G", "W",],
+    #     ["W", "G", "G", "G", "G", "G", "G", "G", "G", "G", "G", ".", "G", "W",],
+    #     ["W", "B", "G", "B", "B", "G", "G", "G", "G", "G", "G", "G", "G", "W",],
+    #     ["W", "G", "G", ".", "B", "G", "G", "G", "G", "G", "G", "G", "G", "W",],
+    #     ["W", "G", "G", "G", ".", "G", "G", ".", "G", "G", "G", "G", "E", "W",],
+    #     ["W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W", "W",]
+    # ].copy()
 CELL_NBX = 20
 CELL_NBY = 10
 CELL_SIZE = 32
 HALF_SIZE = CELL_SIZE//2
 WIDTH_WINDOW = CELL_SIZE * CELL_NBX
 HEIGHT_WINDOW = CELL_SIZE * CELL_NBY
+DIRECTIONS = ["Right", "Left", "Up", "Down"]
 ###########################################
 
 ########## Very Useful fonctions ##########
@@ -34,26 +36,52 @@ def sumTuple(a,b):
 
 ########################################### 
 
-def getDirection():
+def loadLevel(level):
+    level = level.split("\n")
+    levelLst = []
+    # add option
+    levelLst.append(level[0].split())
+    # add map
+    for i in range(1,len(level)):
+        levelLst.append(list(level[i]))
+    return levelLst
+
+def getDirection(debug=False):
     direction = 0
     ev=donne_evenement()
     type_ev=type_evenement(ev)
-    if type_ev=="Touche":
-        t=touche(ev)
-        if t=="Right":
+    if type_ev=="Touche" or debug:
+        if debug:
+            t=DIRECTIONS[randint(0,3)]
+        else:
+            t=touche(ev)
+        if t==DIRECTIONS[0]:
             direction=(1,0)
-        elif t=="Left":
+        elif t==DIRECTIONS[1]:
             direction=(-1,0)
-        elif t=="Up":
+        elif t==DIRECTIONS[2]:
             direction=(0,-1)
-        elif t=="Down":
+        elif t==DIRECTIONS[3]:
             direction=(0,1)
+        elif t=="r":
+            direction="reset"
+        elif t=="d":
+            direction="debug"
         else:
             direction=(0,0)
     else:
         direction=(0,0)
     return direction
 
+def drawBackground():
+    rectangle(
+        0,
+        0,
+        WIDTH_WINDOW,
+        HEIGHT_WINDOW,
+        'black',
+        'black'        
+    )
 
 def drawVoid(coord):
     rectangle(
@@ -85,7 +113,16 @@ def drawGrass(coord):
         '#850'
     )
 
-def drawBoulder(coord):
+def drawBoulder(coord, dead=False):
+    if dead:
+        rectangle(
+            coord[0],
+            coord[1],
+            coord[0]+CELL_SIZE,
+            coord[1]+CELL_SIZE,
+            '#f00',
+            '#f00'
+        )
     drawVoid(coord)
     cercle(
         coord[0]+HALF_SIZE,
@@ -153,6 +190,7 @@ renderCase = {
 
 def renderCanvas(curMap):
     efface_tout()
+    drawBackground()
     for y in range(1,len(curMap)):
         for x in range(0, len(curMap[y])):
             x1 = x*CELL_SIZE
@@ -171,7 +209,7 @@ def foundFallable(curMap):
         for j in range(len(curMap[i])):
             if curMap[i][j] == "B" or curMap[i][j] == "D":
                 fallables.append((j,i))
-    print(fallables)
+    print("fallables: ",fallables)
     return fallables
 
 def updateFallable(lastFa, newFa, fallables):
@@ -182,89 +220,141 @@ def updateFallable(lastFa, newFa, fallables):
             fallables[i] = newFa
             break
 
+
+
 def getCell(coord, curMap):
     return curMap[coord[1]][coord[0]]
 
 def setCell(coord, curMap, content):
     curMap[coord[1]][coord[0]] = content
 
+def setRockfordCell(lastPos, newPos, curMap, aim="R"):
+    setCell(lastPos, curMap, ".") 
+    setCell(newPos, curMap, aim)
 
 def moveRockford(rockford, direction, curMap, fallables):
     print(rockford)
+    print(fallables)
     aimCoord = sumTuple(rockford, direction)
 
     aimCell = getCell(aimCoord, curMap)
     
     if aimCell == ".":
         print("Hey, it's a void")
-        setCell(rockford, curMap, ".")   # refectoring à faire
-        setCell(aimCoord, curMap, "R")
+        setRockfordCell(rockford, aimCoord, curMap)
         return aimCoord
     elif aimCell == "G":
-        print("Hey, it's a grass")
-        setCell(rockford, curMap, ".")   # refectoring à faire
-        setCell(aimCoord, curMap, "R")
+        print("Dirty dancing!")
+        setRockfordCell(rockford, aimCoord, curMap)
         return aimCoord
     elif aimCell == "B":
-        if direction[1] == 0:
+        if direction[1] == 0: # deplacer boulet uniquement si direction lateral
             behindBoulder = sumTuple(aimCoord,direction)
             if(getCell(behindBoulder, curMap) == "."):
                 print("BOULDER DASH")
-                setCell(rockford, curMap, ".")   # refectoring à faire
-                setCell(aimCoord, curMap, "R")
+                setRockfordCell(rockford, aimCoord, curMap)
                 setCell(behindBoulder, curMap, "B")
                 updateFallable(aimCoord, behindBoulder, fallables)
                 return aimCoord
     elif aimCell == "D":
         print("DIAMOND")
-        setCell(rockford, curMap, ".")   # refectoring à faire
-        setCell(aimCoord, curMap, "R")
+        setRockfordCell(rockford, aimCoord, curMap)
+        fallables.remove((aimCoord[0], aimCoord[1]))
         return aimCoord[0], aimCoord[1], aimCoord[2]+1
-
+    elif aimCell == "E":
+        print("BRAVO!")
+        setRockfordCell(rockford, aimCoord, curMap, aim="E")
+        return "win"
     else:
         print("Useless...", aimCell, getCell(rockford,curMap))
     return rockford
     
-def updatePhysic(fallables, curMap):
+def updatePhysic(fallables, fall, charlie, curMap):
+    """
+    met a jour la physique et return vrai
+    si les fallables peuvent encore tomber
+    """
+    print("FALL")
+    print(fallables)
+    fallingNb = 0
     for i, fa in enumerate(fallables):
         faX = fa[0]
         faY = fa[1]
-        if faY < CELL_NBY:
-            if curMap[faY+1][faX] == ".":
-                setCell((faX, faY+1), curMap, curMap[faY][faX])
-                setCell((faX, faY), curMap, ".")
-                fallables[i] = (faX, faY+1)
+        print(fall, curMap[faY+1][faX])
+        setC = False
+        if fall and curMap[faY+1][faX] == "R":
+            setC = True
+            charlie = "lose"
 
+        if curMap[faY+1][faX] == ".":
+            setC = True
+            fallables[i] = (faX, faY+1)
+            fallingNb+=1
+        
+        if setC:
+            setCell((faX, faY+1), curMap, curMap[faY][faX])
+            setCell((faX, faY), curMap, ".")
 
+    fall = (True if fallingNb else False)
+        
+    print("falling : ", fall)
+    return fall, charlie
 
-def reset():
-    pass
+def start(curMap):
+    charlie = trouveCharlie(curMap)  # refactoring
+    charlie = (charlie[0],charlie[1],0)
+    fallables = foundFallable(curMap)
+    fall = True
+    renderCanvas(curMap)
+    return charlie, fallables, fall
 
 def levelWin():
-    pass
+    texte(WIDTH_WINDOW/4, HEIGHT_WINDOW/2-24, "BRAVO!", "green")
 
 def levelLose():
-    pass
+    texte(WIDTH_WINDOW/4, HEIGHT_WINDOW/2-12, "PERDU...", "red")
+
+def status(charlie):
+    if charlie == "win":
+        levelWin()
+        quitter()
+    elif charlie == "lose":
+        levelLose()
+        quitter()
+
+def quitter():
+    attente_clic_ou_touche()
+    exit("Merci")
 
 def debugMode():
     pass
 
 if __name__ == '__main__':
     cree_fenetre(WIDTH_WINDOW,HEIGHT_WINDOW)
-    datamap = list(LEVEL_1)
+    LEVEL_1 = "150s 1d\nWWWWWWWWWWWWWW\nWGGGGGG.GGDGBW\nWGBRBGGGGGG.GW\nWGGGGGGGGGG.GW\nWBGBBGGGGGGGGW\nWGG.BGGGGGGGGW\nWGGG.GG.GGGGEW\nWWWWWWWWWWWWWW"
+    currentMap = loadLevel(LEVEL_1)
 
-    charlie = trouveCharlie(datamap)  # refactoring
-    charlie = (charlie[0],charlie[1],0)
-    fallables = foundFallable(datamap)
-    renderCanvas(datamap)
-
+    charlie, fallables, fall = start(currentMap)
+    fall = True
+    debug = False
 
     while True:
-        start = time()
-        direction = getDirection()
+        direction = getDirection(debug)
+        if direction == "reset":
+            currentMap = loadLevel(LEVEL_1)
+            charlie, fallables, fall = start(currentMap)
+            renderCanvas(currentMap)
+            continue
+        if direction == "debug":
+            debug = (False if debug else True)
+            print ("DEBUG ACTIVATED" if debug else "DEBUG DEACTIVATED")
+            continue
         if(direction[0] != 0 or direction[1] != 0):
-            charlie = moveRockford(charlie, direction, datamap, fallables)
-        updatePhysic(fallables,datamap)
-        renderCanvas(datamap)
+            charlie = moveRockford(charlie, direction, currentMap, fallables)
+            fall, charlie = updatePhysic(fallables, False, charlie, currentMap)
+        if fall:
+            fall, charlie = updatePhysic(fallables, fall, charlie, currentMap)
+        renderCanvas(currentMap)
         mise_a_jour()
-        end = time()
+        status(charlie)
+        
